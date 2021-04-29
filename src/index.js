d73791ef89351e14ee250e3b2eadbef6a5bc6c67
Laurelin <ladylaurel18@gmail.com>
2021-04-29 17:06:37 -0400

    seed file

--- src/index.js
+++ src/index.js
@@ -3,3 +3,11 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+//client side rendered app: create react
+    // database Firebase
+    // react loading skeleton
+    // tailwind
+
+//architecture
+    //components, constants, context, helpers, lib, services, styles(app, tailwind)
