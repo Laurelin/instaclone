2f6d1a1ea5e7c53612d82a20a51deb69efa09c72
Laurelin <ladylaurel18@gmail.com>
2021-05-01 18:04:22 -0400

    seeded database

---
+++ src/lib/firebase.js
@@ -0,0 +1,28 @@
+/** @format */
+
+import Firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+//  import the seed file
+
+import { seedDatabase } from '../seed';
+
+const config = {
+  apiKey: 'AIzaSyCM96hJKsQbOfoqIDj5U3ZUJ6D9OQGx-4c',
+  authDomain: 'instaclone-1e48d.firebaseapp.com',
+  projectId: 'instaclone-1e48d',
+  storageBucket: 'instaclone-1e48d.appspot.com',
+  messagingSenderId: '424272809639',
+  appId: '1:424272809639:web:231276bb9f83d696799582'
+};
+
+const firebase = Firebase.initializeApp(config);
+const { FieldValue } = Firebase.firestore;
+
+//  call the seed file - ONCE
+//seedDatabase(firebase);
+
+console.log('firebase', firebase);
+
+export { firebase, FieldValue };
