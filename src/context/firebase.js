369b747594623d2b2834d719c76c956492a3abce
Laurelin <ladylaurel18@gmail.com>
2021-05-18 22:40:36 -0400

    header, interactive dashboard

--- src/context/firebase.js
+++ src/context/firebase.js
@@ -4,12 +4,16 @@ const FirebaseContext = createContext(null);
 // get a bunch of functions from Firebase that allow you insert info
 // need to like photos, etc
 /*
-
--------component 1 --------
+components are all pages
+context is provider and consumer
+provider-------component 1 -------- (firebase init here)
 -------component 2 --------
 -------component 3 --------
--------component 4 --------
+consumer-------component 4 -------- (firebase init here)
 -------component 5 --------
 -------component 6 --------
+-------component 7 --------
+-------component 8 --------
+consumer-------component 9 -------- (firebase init here)
 */
 export default FirebaseContext;
