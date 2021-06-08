21169a0fa69cc3e324613940e117c59ce9f9b643
Laurelin <ladylaurel18@gmail.com>
2021-06-08 19:06:08 -0400

    got photos from firebase, loading skeleton

---
+++ src/hooks/use-photos.js
@@ -0,0 +1,28 @@
+import { useEffect, useState, useContext } from 'react';
+import UserContext from '../context/user';
+import { getUserByUserId, getPhotos } from '../services/firebase';
+
+export default function usePhotos() {
+  const [photos, setPhotos] = useState(null);
+  const {
+    user: { uid: userId = '' }
+  } = useContext(UserContext);
+
+  useEffect(() => {
+    async function getTimelinePhotos() {
+      const [{ following }] = await getUserByUserId(userId);
+      let followedUserPhotos = [];
+
+      if (following.length > 0) {
+        followedUserPhotos = await getPhotos(userId, following);
+      }
+
+      followedUserPhotos.sort((a, b) => b.dateCreated - a.dateCreated);
+      setPhotos(followedUserPhotos);
+    }
+
+    getTimelinePhotos();
+  }, []);
+
+  return { photos };
+}
